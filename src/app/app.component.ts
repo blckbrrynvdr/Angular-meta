504d882361d1c69a2b07bc90f8a1b52803143104
DESKTOP-GH6HV40\v8a8rt <v8a8rt@gmail.com>
2021-10-04 23:07:18 +0300

    feat(props) passing data in child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
        
 @Component({
     selector: 'my-app',
-    template: `<h2>Количество кликов: {{clicks}}</h2>
-              <child-comp (onChanged)="onChanged($event)"></child-comp>`
+    template: `<child-comp [userName]="name" [userAge]="age"></child-comp>
+                <input type="number" [(ngModel)]="age" />`
 })
 export class AppComponent { 
-     
-    clicks:number = 0;
-    onChanged(increased:any){
-        increased==true?this.clicks++:this.clicks--;
-    }
+    name:string="Tom";
+    age:number = 24;
 }
