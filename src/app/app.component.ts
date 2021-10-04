0ffbb37161aac0011ef898ecfb37570dfd099476
DESKTOP-GH6HV40\v8a8rt <v8a8rt@gmail.com>
2021-10-04 22:51:22 +0300

    взаимодействие между компонентами

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-     
+       
 @Component({
     selector: 'my-app',
-    template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
+    template: `<child-comp [userName]="name" [userAge]="age"></child-comp>
+                <input type="number" [(ngModel)]="age" />`
 })
 export class AppComponent { 
-    name= '';
+    name:string="Tom";
+    age:number = 24;
 }
