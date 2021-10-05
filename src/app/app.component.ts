ee6acef94b3c7d4076a0b72fd56a13c9d7f00b2d
DESKTOP-GH6HV40\v8a8rt <v8a8rt@gmail.com>
2021-10-05 17:58:53 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component} from '@angular/core';
-    
+ 
 @Component({
-    selector: 'my-app',
-    template: `<div>
-                    <h1>AppModule</h1>
-                    <data-comp></data-comp>
-               </div>`
+    selector: 'my-app',
+    template: `<div>
+                  <p bold>Hello Angular</p>
+                  <p>Angular представляет модульную архитектуру приложения</p>
+               </div>`
 })
 export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule }      from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import { DataModule }   from './data/data.module';
-   
+import { AppComponent }   from './app.component';
+import { BoldDirective} from './bold.directive';
+ 
 @NgModule({
-    imports:      [ BrowserModule, DataModule],
-    declarations: [ AppComponent],
-    bootstrap:    [ AppComponent ]
+    imports:      [ BrowserModule],
+    declarations: [ AppComponent, BoldDirective],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
